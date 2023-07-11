f117e640a103386766405b435385524363f7d61f
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-11 13:11:37 +0300

    Create Challenge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import "./App.css";
+import "./styles/App.css";
+import Challenge from "./components/Challenge";
+import image from "./assets/beach.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Where's Waldo</header>
+      <Challenge url={image}></Challenge>
     </div>
   );
 }
---
+++ src/components/Challenge.js
@@ -0,0 +1,7 @@
+export default function Challenge({ url }) {
+  return (
+    <div className="challenge">
+      <img src={url} alt="" />
+    </div>
+  );
+}
