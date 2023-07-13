7890de737a785be658d8ad089293cc3fafea023b
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-13 09:19:56 +0300

    Refactor data module

--- src/utils/correctSquares.js
+++
@@ -1,5 +0,0 @@
-const data = { beach: { odlaw: [2, 21], waldo: [48, 22], wizard: [16, 20] } };
-
-export default function correctSquares(name) {
-  return data[name];
-}
---
+++ src/utils/data.js
@@ -0,0 +1,8 @@
+const corretSquares = {
+  beach: { odlaw: [2, 21], waldo: [48, 22], wizard: [16, 20] },
+};
+
+const getCorrectSquares = (name) => corretSquares[name];
+
+const data = { getCorrectSquares };
+export default data;
