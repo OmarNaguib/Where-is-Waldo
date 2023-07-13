5a3568dace96d64a6a3ddbbc6a9babb6c59af971
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-13 14:22:09 +0300

    Change header on found

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 export default function Header({ characters }) {
   const headerCharacters = characters.map((item) => {
     return (
-      <div className="character">
+      <div className={`character ${item.found ? "found" : ""}`}>
         <img src={item[1]} alt={item[0]} />
         <div className="name">{item[0]}</div>
+        <div className={`tint ${item.found ? "active" : ""}`}></div>
       </div>
     );
   });
