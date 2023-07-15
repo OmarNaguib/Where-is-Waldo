3a8c2bc2274402396516cd6f9219b43cba30ca2a
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-15 22:41:08 +0300

    Add modal content

--- src/components/EndMessage.js
+++ src/components/EndMessage.js
@@ -2,7 +2,16 @@ import "../styles/modal.css";
 export default function EndMessage() {
   return (
     <div className="end-message modal">
-      <div className="message-content modal-content"></div>
+      <div className="message-content modal-content">
+        <div>Submit your time for the leaderboard</div>
+        <form>
+          <div className="input">
+            <label htmlFor="user">name:</label>
+            <input type="text" id="user" />
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
