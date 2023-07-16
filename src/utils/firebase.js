2743a1072fb96c51feccc688471f8f9311977b39
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-16 16:55:00 +0300

    Install and connect firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlsGE0cYiDJXGS8oafJIQsKPSI5Cb4SR0",
+  authDomain: "waldo-96e4f.firebaseapp.com",
+  projectId: "waldo-96e4f",
+  storageBucket: "waldo-96e4f.appspot.com",
+  messagingSenderId: "997329373398",
+  appId: "1:997329373398:web:4a338a8c0370fe5e4f6177",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
