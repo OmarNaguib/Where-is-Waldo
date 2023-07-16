80967d382f379f6f2af0fb37651fe6569daa48da
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-17 01:54:55 +0300

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./styles/App.css";
+import Home from "./components/Home";
 import Challenge from "./components/Challenge";
+import Leaderboard from "./components/LeaderBoard";
 import image from "./assets/beach.jpg";
-
+import { HashRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Challenge url={image} name="beach" startTime={new Date()}></Challenge>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/play"
+            element={
+              <Challenge
+                url={image}
+                name="beach"
+                startTime={new Date()}
+              ></Challenge>
+            }
+          ></Route>
+          <Route path="/board" element={<Leaderboard />}></Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
