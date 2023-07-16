00052cdad32630f6a93ab02377d0af7941158ae7
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-16 15:10:35 +0300

    Layout EndMessage

--- src/components/EndMessage.js
+++ src/components/EndMessage.js
@@ -4,20 +4,26 @@ import "../styles/modal.css";
 export default function EndMessage({ time }) {
   const difference = differenceInSeconds(new Date(), time);
   const minutes = Math.floor(difference / 60);
-  const seconds = difference % 60;
+  const seconds =
+    difference % 60 > 9 ? difference % 60 : "0" + (difference % 60);
   return (
     <div className="end-message modal">
       <div className="message-content modal-content">
-        <div>Submit your time for the leaderboard</div>
+        <div className="prompt">Submit your time for the leaderboard</div>
         <form>
           <div className="input">
-            <label htmlFor="user">name:</label>
+            <label htmlFor="user">name: </label>
             <input type="text" id="user" />
-            <button>Submit</button>
           </div>
           <div>
-            <div className="time">{`${minutes} : ${seconds}`}</div>
+            <label htmlFor="time">time: </label>
+            <output
+              id="time"
+              className="time"
+            >{`${minutes} : ${seconds}`}</output>
           </div>
+
+          <button>Submit</button>
         </form>
       </div>
     </div>
