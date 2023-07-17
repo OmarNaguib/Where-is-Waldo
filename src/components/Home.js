dfd6f55b4737c3bc76ebe556baf2fdc878812a9e
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-17 12:20:32 +0300

    Create home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,13 @@
+import { NavLink } from "react-router-dom";
 export default function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="play">
+        <NavLink to="/play">Play</NavLink>
+      </div>
+      <div className="leaderboard">
+        <NavLink to="/leaderboard">Leaderboard</NavLink>
+      </div>
+    </div>
+  );
 }
